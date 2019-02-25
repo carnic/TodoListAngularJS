cde6ca8598c86cce269591c5d108d27c1448fbe0
carol_pereira <carol_pereira@persistent.co.in>
2019-02-25 14:41:08 +0530

    fourth commit

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-var app = angular.module("todoList", []);
+var app = angular.module("todoList", ["ngRoute"]);
+
+app.config(function($routeProvider){
+    $routeProvider
+    .when("/", {
+        templateUrl: "view/todolist.htm",
+        controller: "todoListCtrl"
+    })
+    .when("/about",{
+        templateUrl: "view/about.htm"
+    });
+});
--- controller/todoListCtrl.js
+++ controller/todoListCtrl.js
@@ -1,36 +1,59 @@
-app.controller("todoListCtrl", function($scope, todoService){
+app.controller("todoListCtrl", function($rootScope, $scope, todoService){
     $scope.name = "Carol";
     $scope.itemTitle = "";
+    $scope.error=false;
+    $scope.status=false;
 
     $scope.init = function(){
-        todoService.getList().then(function(res){
-            $scope.todolist = res.data;
-        });
+        if($rootScope.todolist == undefined){
+            todoService.getList().then(function(res){
+                $rootScope.todolist = res.data;
+            });
+        }
     }
 
     $scope.deleteItem = function(i){
-        todoService.deleteItem($scope.todolist[i].id).then(function(res){
+        todoService.deleteItem($rootScope.todolist[i].id).then(function(res){
             if(res.status == 200){
-                $scope.todolist.splice(i,1);
+                $rootScope.todolist.splice(i,1);
             }
         });
     }
 
     $scope.addItem = function(){
-        if($scope.itemTitle !== undefined){
-            var arr = {title: $scope.itemTitle, description: "TestDesc", completed: false};
+        if($scope.itemTitle != undefined && $scope.itemTitle != ""){
+            var arr = {title: $scope.itemTitle, completed: false};
             todoService.addItem(arr).then(function(res){
                 if(res.status == 201){
-                    $scope.todolist.push(arr);
+                    $rootScope.todolist.push(arr);
+                    $scope.errorMessage="Item is added.";
+                    $scope.error=false;
                 }
             });
         }
+        else{
+            $scope.errorMessage="Item Title should be entered.";
+            $scope.error=true;
+        }
+        $scope.status=true;
+    }    
+
+    $scope.selectAll = function(){
+        $rootScope.todolist.forEach(function(item, index){
+            console.log(item.id);
+            item.completed = true;
+            todoService.updateItem(item.id, item).then(function(res){
+                if(res.status == 200){
+                    $rootScope.todolist[index] = item;
+                }
+            });
+        });
     }
 
     $scope.completedItem = function(i, item){
         todoService.updateItem(item.id, item).then(function(res){
             if(res.status == 200){
-                $scope.todolist[i] = item;
+                $rootScope.todolist[i] = item;
             }
         });
     }
